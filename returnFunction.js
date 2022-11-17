6a6f9f41715fc8c59c71b7d4318fc1c2b01fc17c
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-11-17 20:22:42 +0530

    function return type

---
+++ returnFunction.js
@@ -0,0 +1,13 @@
+//function runMore(distance: number): number {
+//return distance + 10;
+//}
+function eat(calories) {
+    console.log("I ate ".concat(calories, " calories"));
+}
+function sleepIn(hours) {
+    console.log("I slept in ".concat(hours, " hours"));
+}
+var ate = eat(100);
+console.log(ate);
+var slept = sleepIn(10);
+console.log(slept);
